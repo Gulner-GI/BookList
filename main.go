7f91f591b9a36e05b0e65bffe66e26bf5bd3ce8e
Gulner-GI <yura.kuzin1990@gmail.com>
2025-05-10 20:31:28 +0300

    Какое-то сообщение
    Я не знаю что делать

--- main.go
+++ main.go
@@ -8,8 +8,24 @@ import (
 
 func main() {
 	r := gin.Default()
+	var todos = []Todo{
+		{ID: 1, Task: "Learn Go"},
+	}
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+	r.GET("/todos", func(c *gin.Context) {
+		c.JSON(http.StatusOK, todos)
+	})
+	r.POST("/todos", func(c *gin.Context) {
+		var newTodo Todo
+		if err := c.BindJSON(&newTodo); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		newTodo.ID = len(todos) + 1
+		todos = append(todos, newTodo)
+		c.JSON(http.StatusCreated, newTodo)
 	})
 	r.Run()
 }
