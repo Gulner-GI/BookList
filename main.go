dd457f309f364b0a60dd0cd023328e9b768e01aa
Gulner-GI <yura.kuzin1990@gmail.com>
2025-05-11 10:21:49 +0300

    Full CRUD

--- main.go
+++ main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"net/http"
+	"slices"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
+func findtodo(id int) int {
+	for i, t := range todos {
+		if t.ID == id {
+			return i
+		}
+	}
+	return -1
+}
+
+func removetodo(idx int) {
+	todos = slices.Delete(todos, idx, idx+1)
+}
+
 func main() {
 	r := gin.Default()
-	var todos = []Todo{
-		{ID: 1, Task: "Learn Go"},
-	}
-	r.GET("/todos", func(c *gin.Context) {
-		c.JSON(http.StatusOK, todos)
+	r.GET("/todos/:id", func(c *gin.Context) {
+		idParam := c.Param("id")
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+			return
+		}
+		idx := findtodo(id)
+		if idx == -1 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "todo not found"})
+			return
+		}
+		c.JSON(http.StatusOK, todos[idx])
 	})
+
 	r.POST("/todos", func(c *gin.Context) {
 		var newTodo Todo
 		if err := c.BindJSON(&newTodo); err != nil {
@@ -21,8 +45,55 @@ func main() {
 			return
 		}
 		newTodo.ID = len(todos) + 1
+		newTodo.Done = false
 		todos = append(todos, newTodo)
 		c.JSON(http.StatusCreated, newTodo)
 	})
+
+	r.PATCH("/todos/:id", func(c *gin.Context) {
+		idParam := c.Param("id")
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+			return
+		}
+		var updatedTodo struct {
+			Task *string `json:"task,omitempty"`
+			Done *bool   `json:"done,omitempty"`
+		}
+		if err := c.BindJSON(&updatedTodo); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		idx := findtodo(id)
+		if idx == -1 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "todo not found"})
+			return
+		}
+		if updatedTodo.Task != nil {
+			todos[idx].Task = *updatedTodo.Task
+		}
+		if updatedTodo.Done != nil {
+			todos[idx].Done = *updatedTodo.Done
+		}
+		c.JSON(http.StatusOK, todos[idx])
+	})
+
+	r.DELETE("/todos/:id", func(c *gin.Context) {
+		idParam := c.Param("id")
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "ivalid id"})
+			return
+		}
+		idx := findtodo(id)
+		if idx == -1 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "todo not found"})
+			return
+		}
+		removetodo(idx)
+		c.Status(http.StatusNoContent)
+	})
+
 	r.Run()
 }
--- models.go
+++ models.go
@@ -3,4 +3,9 @@ package main
 type Todo struct {
 	ID   int    `json:"id"`
 	Task string `json:"task"`
+	Done bool   `json:"done"`
+}
+
+var todos = []Todo{
+	{ID: 1, Task: "Learn Go", Done: false},
 }
